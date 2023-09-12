cfb03e073ccce98f06421d3098a23998f32112ac
Ignacio Crusoe <icrusoe@frba.utn.edu.ar>
2023-09-12 20:51:38 +0000

    Avances conexiones

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,7 +1,18 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include "./../../shared/src/log_utils.c"
+#include "./../../shared/src/server_utils.c"
 
-int main(int argc, char* argv[]) {
-    
-    return 0;
+
+int main(int argc, char* argv[]){
+    t_log* logger = iniciar_logger("log_memoria.log","CPU");
+    t_config* config = iniciar_config("./cfg/memoria.config");
+    char* puerto_escucha;
+    puerto_escucha = config_get_string_value(config,"PUERTO_ESCUCHA");
+    printf("PUERTO_ESCUCHA=%s\n",puerto_escucha);
+
+    int socket_servidor = iniciar_servidor(logger,puerto_escucha);
+    int socket_kernel = esperar_cliente(logger, socket_servidor);
 }
